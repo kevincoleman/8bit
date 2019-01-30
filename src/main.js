7ffdecebffdd027fc8fb9d56e9a94cd5977e3613
Nathan Isaac <nathanjisaac@gmail.com>
2019-01-29 19:20:04 -0800

    Add debug stats

--- src/main.js
+++ src/main.js
@@ -1,20 +1,40 @@
-
+import Stats from 'stats.js';
 import * as THREE from 'three';
+
+const stats = new Stats();
+stats.showPanel(1);
+document.body.append(stats.dom);
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 var renderer = new THREE.WebGLRenderer();
+
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 camera.position.z = 5;
-var animate = function () {
-    requestAnimationFrame( animate );
+
+
+const update = () => {
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+};
+
+const render = () => {
     renderer.render( scene, camera );
 };
-animate();
+
+const gameLoop = () => {
+  stats.begin();
+  update();
+  render();
+  stats.end();
+
+  requestAnimationFrame( gameLoop );
+};
+
+gameLoop();
